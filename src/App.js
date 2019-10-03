d2379983decb24cd84808ca9994b8363a5efb020
Your Name <kevincarr@Kevins-MacBook-Pro.local>
2019-10-02 19:19:12 -0500

    rendered list to screen

--- src/App.js
+++ src/App.js
@@ -1,13 +1,55 @@
 import React from 'react';
+import TodoList from './components/TodoComponents/TodoList'
+
 
 class App extends React.Component {
+  constructor() {
+    super()
+
+    this.state = {
+
+      todos: [
+        {
+          task: "Walk The Dog",
+          id: Date.now(),
+          completed: false
+        },
+        {
+          task: "Take Out Trash",
+          id: Date.now(),
+          completed: false
+        },
+        {
+          task: "30 Minute Workout",
+          id: Date.now(),
+          completed: false
+        },
+        {
+          task: "Fix Back Door",
+          id: Date.now(),
+          completed: false
+        },
+        {
+          task: "Read a Book",
+          id: Date.now(),
+          completed: false
+        },
+        {
+          task: "Learn Something New",
+          id: Date.now(),
+          completed: false
+        }
+      ]
+    }
+  }
   // you will need a place to store your state in this component.
   // design `App` to be the parent component of your application.
   // this component is going to take care of state, and any change handlers you need to work with your state
+
   render() {
     return (
       <div>
-        <h2>Welcome to your Todo App!</h2>
+        <TodoList todos={this.state.todos} />
       </div>
     );
   }
---
+++ src/components/TodoComponents/Todo.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Todo = props => {
+  return (
+    <div>
+      <p>{props.todo.task}</p>
+    </div>
+  )
+}
+
+export default Todo
+
+
